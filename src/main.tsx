5444bec6448cc2195afb0190129e24d74a2eaab0
Jorge <herreraceciliajorgeluis04@gmail.com>
2026-02-10 03:30:34 -0500

    first commit with all :)

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { LanguageProvider } from './context/LanguageContext'
-import { ThemeProvider } from './context/ThemeContext'
+import { LanguageProvider } from './context/LanguageContext.tsx'
+import { ThemeProvider } from './context/ThemeContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
